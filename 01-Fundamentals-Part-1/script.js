f947ceb574c7c4b45c006093a151210f4d8ea626
athumanrajab <athumanrajab0903@gmail.com>
2026-08-03 17:01:09 +0300

    value and variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,27 @@
 let js = "amaizing";
-      if (js === "amaizing") alert("JavaScript is FUN!");
+if (js === "amaizing") alert("JavaScript is FUN!");
 
-      console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
+
+// Rules for naming variables in JavaScript
+// 1. We use camelCase for variable names in JavaScript
+// 2. Variable names can only contain letters, numbers, underscores, and dollar signs
+// 3. Variable names cannot start with a number
+// 4. Variable names cannot be reserved keywords in JavaScript such as let, function, or const, new
+
+// 5. Variable names should be descriptive and meaningful
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+// 6. Variable names are case-sensitive
+// 7. Variable names should not contain spaces
+// 8. Variable names should not be too long or too short
+// 9. Variable names should not start with uppercase letter because it is reserved for classes and constructors
+// 10. Variable names should not contain special characters such as @, #, %, &, *, etc.
+// 11.Variable names that contain all uppercase letters are usually reserved for constants such as PI, MAX_VALUE, etc.
+
+let firstName = "Jonas";
+console.log(firstName);

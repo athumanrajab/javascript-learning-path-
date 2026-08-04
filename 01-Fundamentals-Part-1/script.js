346cd70364ab07f1c68acb453666836d06f47d10
athumanrajab <athumanrajab0903@gmail.com>
2026-08-04 11:30:54 +0300

    Coding Challenge 1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -70,7 +70,7 @@ console.log(typeof javascriptIsFun)
 console.log(typeof null)
 */
 
-
+/*
 // Variable declaration
 // 1.Use "let" to declare variable that can be changed later in code (Mutable variables)
 
@@ -117,3 +117,44 @@ let hasDance = true;
 
 middlename = "Fayma";
 console.log(middlename);
+*/
+
+
+
+/*
+// Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK 😀
+*/
+
+let markMass , markHeight, johnMass, johnHeight;
+
+markMass = 78;
+markHeight = 1.69;
+johnMass = 92;
+johnHeight = 1.95;
+
+let markBMI = markMass / (markHeight * markHeight);
+
+console.log("markBMI: ", markBMI)
+
+let johnBMI = johnMass / (johnHeight * johnHeight);
+
+console.log("johnBMI: ", johnBMI)
+
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);

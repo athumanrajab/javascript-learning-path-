c3ddc9dafaa15204586ff973ecef4da75e57dd3b
athumanrajab <athumanrajab0903@gmail.com>
2026-08-05 17:23:03 +0300

    Ternary operator + Coding challenge 4

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -451,3 +451,61 @@ Example when we use template literal , we expect an expression and not a stateme
  console.log(`I am ${2026 - 2003} years old`);
 */
 
+// Ternary operator
+// condition ? what will happen if the condtion is true : what will happen if the condition is false;
+
+const age = 23;
+
+age >= 18 ? console.log("I like to drink wine"): console.log("I like to drink water")
+
+// Since ternary opertaor is an expression then can produce a value , this make it possible to store it into variable
+
+const drink = age >= 23 ? "Wine": "Water";
+console.log(drink);
+
+// But also it can be used inside the {} of the template literal because it's an expression
+
+console.log(`I like to drink ${age >= 23 ? "Wine": "Water"}`);
+
+/*
+Coding Challenge #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§
+Data 1: Test for bill values 275, 40 and 430
+Hints:
+§To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+GOOD LUCK 😀
+*/
+
+let tip;
+let total;
+const bill = 40;
+
+// The challenge above can be done by using either ternary operator or if/else statement, but since the challenge is to use ternary operator then we can do it as follows
+
+bill >= 50 && bill <= 300 ? tip = 0.15 * bill: tip = 0.2 * bill;
+total = bill + tip;
+console.log(`The Bill was ${bill}, the Tip was ${tip}, and the total value is ${total}`);
+
+
+// By using if/else statement, the challenge can be done as follows
+if (bill >= 50 && bill <= 300) {
+    tip = 0.15 * bill;
+} else {
+    tip = 0.2 * bill;
+}
+total = bill + tip;
+console.log(`The Bill was ${bill}, the Tip was ${tip}, and the total value is ${total}`);
+

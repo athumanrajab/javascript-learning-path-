7a09fc59d3c27b0a260eed15245df05909973ae6
athumanrajab <athumanrajab0903@gmail.com>
2026-08-28 13:50:49 +0300

    Coding Challenge 3: Data Structures, Modern Operators and Strings

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -854,6 +854,7 @@ rest.set(document.querySelector("h1"), "Heading");
 console.log(rest);
 */
 
+/*
 // Maps Iteration
 // We can directly create a map and fill it with values without even using the set method
 const question = new Map([
@@ -906,3 +907,65 @@ console.log(valuesArr);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 console.log([...question.entries()]);
+*/
+
+/*
+Coding Challenge #3
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+⚽
+[FIRST HALF] 17:
+GOAL
+*/
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// TODO: Task 1
+// Solution 1: We start by first converting the Map into array using the spread operator, and use the values() inorder to only get values from the gameEvent
+// const eventArr = [...gameEvents.values()];
+// console.log(eventArr);
+// Then We can convert the resulted array to a set so as we dont have duplicate values
+// const events = new Set(eventArr);
+// console.log(events);
+
+// Solution 2: we can directly use the spread operator and the values() so as we can directly create a set/array with no duplicate
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+
+// TODO: Task 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// TODO: Task 3
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`,
+);
+
+// TODO: Task 4
+for (const [min, event] of gameEvents) {
+  const x = min <= 45 ? "First Half" : "Second Half";
+  console.log(`[${x}] ${min}: ${event}`);
+}

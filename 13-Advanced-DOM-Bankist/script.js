8679b56f947a5db589bffbe734aa1a85a750b8d0
athumanrajab <athumanrajab0903@gmail.com>
2026-09-14 16:28:29 +0300

    Building a tabbed component

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -71,48 +71,30 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
-// NOTE: DOM traversing - Means walking through the DOM .. which means that we can select an element based on another element
+// TODO: 3.Building a tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContents = document.querySelectorAll(".operations__content");
 
-const h1 = document.querySelector("h1");
-console.log(h1);
+// Lets use event deligation to attach and event listener to the tabsContainer which is the common parent of the three tabs (Instant Transfers , Instant loans and instant closing)
+tabsContainer.addEventListener("click", function (e) {
+  // Matching Strategy : In such a way that when we clicked anywhere inside the tab we should really get that tab
+  const clicked = e.target.closest(".operations__tab");
+  // Trying to handle errors that will be caused when we try to click outside the tabsContainer because the result will be null
+  if (clicked) {
+    // Active tabs
+    tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+    clicked.classList.add("operations__tab--active");
 
-// Going downwards: by selecting all elements of class "highlight" that are children of h1 element
-// NOTE: 1. The first way is to use querySelector() or querySelectorAll() because it works not only the document object but also on the element
-console.log(h1.querySelectorAll(".highlight")); //This will only select all elements that have class "highlight" and are children of h1 elements ,such that if there might be some other elements that are not the children of h1 element but they have class "highlight" they will not be selected. but also it will go as deep as possile to find all children with the given class
-console.log(h1.childNodes); //Returns everything that are child nodes if h1 in the DOM tree
+    // Active Content area
+    const currentArea = clicked.dataset.tab;
 
-// This one work only for direct children
-console.log(h1.children); //Returns the HTML collections of only the child elements of h1
+    tabsContents.forEach((content) =>
+      content.classList.remove("operations__content--active"),
+    );
 
-// We can also get and set properties of first child and the last child of the given element by using firstElementChild  and lastElementChild properties
-// first child
-console.log(h1.firstElementChild.textContent);
-h1.firstElementChild.style.color = "orangered";
-
-// last child
-console.log(h1.lastElementChild.textContent);
-h1.lastElementChild.style.color = "powderblue";
-
-// Going upwards: By basically selecting parents
-console.log(h1.parentNode); //Returns the parent node of the specified element
-
-console.log(h1.parentElement); //Return the direct parent element of h1
-
-// In most of the time we need to find the parent element regardless of how far does the parent is, by using closest()
-console.log(h1.closest(".header"));
-h1.closest(".header").style.background = "var(--gradient-secondary)";
-// NOTE: querySelector finds children no matter how deep they are nested while closest find parent no matter how far it's  in the DOM tree
-
-// Going sideways: which means selecting siblings
-// We can only access direct siblings which are previous and next siblings
-// The below methods will return an element if such sibling exist or null if there is no sibling
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
-
-// If we really want to get all other siblings of that element we can use the below trick
-// TRICK: we can take the element that we can find it's parent element and then after get the parent element we can use the children properties which return an htmlCollection of all elements including that element
-console.log(h1.parentElement.children);
-
-// But also we can get previously and next sibling nodes
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
+    document
+      .querySelector(`.operations__content--${currentArea}`)
+      .classList.add("operations__content--active");
+  }
+});

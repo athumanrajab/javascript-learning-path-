7b2546e33778f36b3fee327027a084f072530214
athumanrajab <athumanrajab0903@gmail.com>
2026-09-24 16:31:38 +0300

    Coding Challenge #1: Object Oriented Programming (OOP)

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -123,3 +123,49 @@ Array.prototype.unique = function () {
 };
 // This is just for fun experiment, but avoid manipulate the prototype of built-in constructor functions
 console.log(arr.unique());
+
+/*
+Object Oriented Programming (OOP)
+Coding Challenge #1
+Your tasks:
+1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+'speed' property. The 'speed' property is the current speed of the car in
+km/h
+2. Implement an 'accelerate' method that will increase the car's speed by 10,
+and log the new speed to the console
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+the new speed to the console
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+'brake' multiple times on each of them
+Test data:
+Data car 1: 'BMW' going at 120 km/h
+Data car 2: 'Mercedes' going at 95 km/h
+GOOD LUCK
+*/
+
+// NOTE: Coding Challenge #1: Object Oriented Programming (OOP)
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`The new ${this.make}'s speed is: ${this.speed}km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`The new ${this.make}'s speed is: ${this.speed}km/h`);
+};
+
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 95);
+
+// For car1
+car1.accelerate();
+car1.brake();
+
+// For car2
+car2.accelerate();
+car2.brake();
